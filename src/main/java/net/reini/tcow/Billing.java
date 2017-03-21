97bf496fcacbd08a208a1312d68267dd8109fa2c
Patrick Reinhart <patrick@reini.net>
2017-03-21 20:23:37 +0100

    Reformatted using Google code style

--- src/main/java/net/reini/tcow/Billing.java
+++ src/main/java/net/reini/tcow/Billing.java
@@ -39,10 +39,8 @@ import java.util.stream.Collectors;
 
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
-import javax.mail.Authenticator;
 import javax.mail.MessagingException;
 import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -68,263 +66,243 @@ import com.lowagie.text.pdf.PdfStamper;
 import com.sun.mail.smtp.SMTPMessage;
 
 public class Billing implements AutoCloseable {
-	private static final String RECHNUNG = "Rechnung";
+  private static final String RECHNUNG = "Rechnung";
 
-	public static void main(String[] args) {
-		int argsLength = args.length;
-		if (argsLength >= 1) {
-			try (Billing billing = new Billing(args[0])) {
-				if (argsLength >= 3) {
-					Path pdfFile = Paths.get(args[0]).resolve(args[1]);
-					Map<String, String> row = new HashMap<>();
-					row.put("Email", args[2]);
-					for (int idx = 3; idx < argsLength; idx++) {
-						String[] parts = args[idx].split("=", 2);
-						row.put(parts[0], parts[1]);
-					}
-					billing.sendEmail(row, pdfFile);
-				} else {
-					billing.readBillingInformation(billing::processDocument);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.out.println(
-					"Usage: TCOW <datadirectory> [<billingPdfFile> <targetEmail> [<rowkey=rowvalue> ..]]");
-		}
-	}
+  public static void main(String[] args) {
+    int argsLength = args.length;
+    if (argsLength >= 1) {
+      try (Billing billing = new Billing(args[0])) {
+        if (argsLength >= 3) {
+          Path pdfFile = Paths.get(args[0]).resolve(args[1]);
+          Map<String, String> row = new HashMap<>();
+          row.put("Email", args[2]);
+          for (int idx = 3; idx < argsLength; idx++) {
+            String[] parts = args[idx].split("=", 2);
+            row.put(parts[0], parts[1]);
+          }
+          billing.sendEmail(row, pdfFile);
+        } else {
+          billing.readBillingInformation(billing::processDocument);
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    } else {
+      System.out.println(
+          "Usage: TCOW <datadirectory> [<billingPdfFile> <targetEmail> [<rowkey=rowvalue> ..]]");
+    }
+  }
 
-	private final Logger logger;
-	private final LocalDateTime date;
-	private final Session mailSession;
-	private final DateTimeFormatter dateFormatter;
-	private final DateTimeFormatter yearFormatter;
-	private final Path dataDir;
-	private final InternetAddress sender;
-	private final Pattern replacementPattern;
-	private final PdfCopyFields pdfCopy;
+  private final Logger logger;
+  private final LocalDateTime date;
+  private final Session mailSession;
+  private final DateTimeFormatter dateFormatter;
+  private final DateTimeFormatter yearFormatter;
+  private final Path dataDir;
+  private final InternetAddress sender;
+  private final Pattern replacementPattern;
+  private final PdfCopyFields pdfCopy;
+  private final Properties mailProperties;
 
-	public Billing(String dataDirectory) throws IOException, DocumentException {
-		dataDir = Paths.get(dataDirectory);
-		logger = LoggerFactory.getLogger(getClass());
-		date = LocalDateTime.now();
-		dateFormatter = ofPattern("dd. MMMM yyyy", Locale.GERMAN);
-		yearFormatter = ofPattern("yyyy", Locale.GERMAN);
-		sender = new InternetAddress("patrick@reini.net", "Patrick Reinhart");
-		replacementPattern = Pattern.compile("\\$\\{([\\w]+)\\}");
-		pdfCopy = new PdfCopyFields(
-				newOutputStream(dataDir.resolve(RECHNUNG + "enToPrint.pdf")));
-		Properties props = new Properties();
-		Path mailProperties = Paths.get("mail.properties");
-		if (exists(mailProperties)) {
-			try (InputStream in = newInputStream(mailProperties)) {
-				props.load(in);
-			}
-		} else {
-			logger.info("{} not available. Using defaults.", mailProperties);
-		}
-		mailSession = Session.getDefaultInstance(props, new Authenticator() {
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(
-						props.getProperty("mail.user"),
-						props.getProperty("mail.password"));
-			}
-		});
-	}
+  public Billing(String dataDirectory) throws IOException, DocumentException {
+    dataDir = Paths.get(dataDirectory);
+    logger = LoggerFactory.getLogger(getClass());
+    date = LocalDateTime.now();
+    dateFormatter = ofPattern("dd. MMMM yyyy", Locale.GERMAN);
+    yearFormatter = ofPattern("yyyy", Locale.GERMAN);
+    sender = new InternetAddress("patrick@reini.net", "Patrick Reinhart");
+    replacementPattern = Pattern.compile("\\$\\{([\\w]+)\\}");
+    pdfCopy = new PdfCopyFields(newOutputStream(dataDir.resolve(RECHNUNG + "enToPrint.pdf")));
+    mailProperties = new Properties();
+    Path mailPropertiesFile = Paths.get("mail.properties");
+    if (exists(mailPropertiesFile)) {
+      try (InputStream in = newInputStream(mailPropertiesFile)) {
+        mailProperties.load(in);
+      }
+    } else {
+      logger.info("{} not available. Using defaults.", mailPropertiesFile);
+    }
+    mailSession = Session.getDefaultInstance(mailProperties);
+  }
 
-	@Override
-	public void close() throws Exception {
-		pdfCopy.close();
-	}
+  @Override
+  public void close() throws Exception {
+    pdfCopy.close();
+  }
 
-	Map<String, Object> processDocument(Map<String, String> row) {
-		String vorname = get("Vorname", row);
-		String nachname = get("Name", row);
-		String anrede = get("Anrede", row);
-		String strasse = get("Strasse", row);
-		String plz = get("PLZ", row);
-		String ort = get("Ort", row);
-		String bezeichnung = get("Bezeichnung", row);
-		String betrag = get("Betrag", row);
-		String email = get("Email", row);
-		String typ = get("Typ", row);
-		String vornameEinzeln = vorname.replaceFirst(" & .+", "").concat(",");
-		String postAdresse = format("%s\n%s %s\n%s\n%s %s", anrede, vorname,
-				nachname, strasse, plz, ort);
-		String bezahltAm = get("Bezahlt", row);
-		try {
-			Path pdfFile = createDirectories(dataDir.resolve(RECHNUNG + "en"))
-					.resolve(format("%s_%s_%s.pdf", RECHNUNG, vorname, nachname)
-							.replace(' ', '_'));
-			if (!exists(pdfFile)) {
-				PdfReader pfdReader = createReader(typ);
-				logger.info("Creating PDF {}", pdfFile);
-				PdfStamper pdfStamper = new PdfStamper(pfdReader,
-						newOutputStream(pdfFile));
-				AcroFields form = pdfStamper.getAcroFields();
-				form.setField("Adresse", postAdresse);
-				form.setField("Vorname", vornameEinzeln);
-				form.setField("Jahr1", date.format(yearFormatter));
-				form.setField("Jahr2", date.format(yearFormatter));
-				form.setField("Datum", date.format(dateFormatter));
-				form.setField("Bezeichnung", bezeichnung);
-				form.setField("Betrag", betrag);
-				if (!bezahltAm.isEmpty()) {
-					PdfContentByte canvas = pdfStamper.getOverContent(1);
-					Phrase phrase = new Phrase(
-							format("Betrag erhalten am %s", bezahltAm));
-					phrase.getFont().setColor(Color.BLUE);
-					ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, phrase,
-							380, 50, 10);
-				}
-				pdfStamper.close();
-			}
-			if (email.isEmpty()) {
-				try (InputStream in = newInputStream(pdfFile)) {
-					pdfCopy.addDocument(new PdfReader(in));
-				}
-			} else if (row.get("Mailed").isEmpty()) {
-				return sendEmail(row, pdfFile);
-			}
-		} catch (IOException | DocumentException | MessagingException e) {
-			logger.error("Error processing PDF", e);
-		}
-		return Collections.emptyMap();
-	}
+  Map<String, Object> processDocument(Map<String, String> row) {
+    String vorname = get("Vorname", row);
+    String nachname = get("Name", row);
+    String anrede = get("Anrede", row);
+    String strasse = get("Strasse", row);
+    String plz = get("PLZ", row);
+    String ort = get("Ort", row);
+    String bezeichnung = get("Bezeichnung", row);
+    String betrag = get("Betrag", row);
+    String email = get("Email", row);
+    String typ = get("Typ", row);
+    String vornameEinzeln = vorname.replaceFirst(" & .+", "").concat(",");
+    String postAdresse =
+        format("%s\n%s %s\n%s\n%s %s", anrede, vorname, nachname, strasse, plz, ort);
+    String bezahltAm = get("Bezahlt", row);
+    try {
+      Path pdfFile = createDirectories(dataDir.resolve(RECHNUNG + "en"))
+          .resolve(format("%s_%s_%s.pdf", RECHNUNG, vorname, nachname).replace(' ', '_'));
+      if (!exists(pdfFile)) {
+        PdfReader pfdReader = createReader(typ);
+        logger.info("Creating PDF {}", pdfFile);
+        PdfStamper pdfStamper = new PdfStamper(pfdReader, newOutputStream(pdfFile));
+        AcroFields form = pdfStamper.getAcroFields();
+        form.setField("Adresse", postAdresse);
+        form.setField("Vorname", vornameEinzeln);
+        form.setField("Jahr1", date.format(yearFormatter));
+        form.setField("Jahr2", date.format(yearFormatter));
+        form.setField("Datum", date.format(dateFormatter));
+        form.setField("Bezeichnung", bezeichnung);
+        form.setField("Betrag", betrag);
+        if (!bezahltAm.isEmpty()) {
+          PdfContentByte canvas = pdfStamper.getOverContent(1);
+          Phrase phrase = new Phrase(format("Betrag erhalten am %s", bezahltAm));
+          phrase.getFont().setColor(Color.BLUE);
+          ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, phrase, 380, 50, 10);
+        }
+        pdfStamper.close();
+      }
+      if (email.isEmpty()) {
+        try (InputStream in = newInputStream(pdfFile)) {
+          pdfCopy.addDocument(new PdfReader(in));
+        }
+      } else if (row.get("Mailed").isEmpty()) {
+        return sendEmail(row, pdfFile);
+      }
+    } catch (IOException | DocumentException | MessagingException e) {
+      logger.error("Error processing PDF", e);
+    }
+    return Collections.emptyMap();
+  }
 
-	PdfReader createReader(String typ) throws IOException {
-		switch (typ) {
-		default:
-			return new PdfReader(
-					getClass().getResourceAsStream("Mitglied.pdf"));
-		case "3":
-			return new PdfReader(getClass().getResourceAsStream("LuftAbo.pdf"));
-		}
-	}
+  PdfReader createReader(String typ) throws IOException {
+    switch (typ) {
+      default:
+        return new PdfReader(getClass().getResourceAsStream("Mitglied.pdf"));
+      case "3":
+        return new PdfReader(getClass().getResourceAsStream("LuftAbo.pdf"));
+    }
+  }
 
-	void readBillingInformation(
-			Function<Map<String, String>, Map<String, Object>> rowFunction)
-			throws Exception {
-		final AtomicBoolean hasChanged = new AtomicBoolean();
-		SpreadSheet spreadSheet = SpreadSheet.createFromFile(
-				dataDir.resolve("Rechnungsliste_Budget.ods").toFile());
-		try {
-			Sheet sheet = spreadSheet.getSheet(0);
-			List<String> keys = new ArrayList<>();
-			final Map<String, Integer> columnNameMap = new HashMap<>();
-			for (int x = 0, mx = sheet.getColumnCount(); x < mx; x++) {
-				String keyName = sheet.getCellAt(x, 0).getTextValue();
-				if (keyName.isEmpty()) {
-					break;
-				}
-				keys.add(keyName);
-				columnNameMap.put(keyName, Integer.valueOf(x));
-			}
-			for (int y = 1, n = sheet.getRowCount(); y < n; y++) {
-				String anrede = sheet.getCellAt(0, y).getTextValue();
-				if (anrede.isEmpty()) {
-					break;
-				}
-				Map<String, String> rowValues = new LinkedHashMap<>();
-				rowValues.put(keys.get(0), anrede);
-				for (int x = 1, mx = keys.size(); x < mx; x++) {
-					rowValues.put(keys.get(x),
-							sheet.getCellAt(x, y).getTextValue());
-				}
-				final int row = y;
-				Map<String, Object> changedRows = rowFunction.apply(rowValues);
-				changedRows.forEach((key, value) -> {
-					Integer col = columnNameMap.get(key);
-					if (col != null) {
-						sheet.setValueAt(value, col.intValue(), row);
-						hasChanged.set(true);
-					}
-				});
-			}
-		} finally {
-			if (hasChanged.get()) {
-				spreadSheet.saveAs(dataDir
-						.resolve("Rechnungsliste_Budget_new.ods").toFile());
-			}
-		}
-	}
+  void readBillingInformation(Function<Map<String, String>, Map<String, Object>> rowFunction)
+      throws Exception {
+    final AtomicBoolean hasChanged = new AtomicBoolean();
+    SpreadSheet spreadSheet =
+        SpreadSheet.createFromFile(dataDir.resolve("Rechnungsliste_Budget.ods").toFile());
+    try {
+      Sheet sheet = spreadSheet.getSheet(0);
+      List<String> keys = new ArrayList<>();
+      final Map<String, Integer> columnNameMap = new HashMap<>();
+      for (int x = 0, mx = sheet.getColumnCount(); x < mx; x++) {
+        String keyName = sheet.getCellAt(x, 0).getTextValue();
+        if (keyName.isEmpty()) {
+          break;
+        }
+        keys.add(keyName);
+        columnNameMap.put(keyName, Integer.valueOf(x));
+      }
+      for (int y = 1, n = sheet.getRowCount(); y < n; y++) {
+        String anrede = sheet.getCellAt(0, y).getTextValue();
+        if (anrede.isEmpty()) {
+          break;
+        }
+        Map<String, String> rowValues = new LinkedHashMap<>();
+        rowValues.put(keys.get(0), anrede);
+        for (int x = 1, mx = keys.size(); x < mx; x++) {
+          rowValues.put(keys.get(x), sheet.getCellAt(x, y).getTextValue());
+        }
+        final int row = y;
+        Map<String, Object> changedRows = rowFunction.apply(rowValues);
+        changedRows.forEach((key, value) -> {
+          Integer col = columnNameMap.get(key);
+          if (col != null) {
+            sheet.setValueAt(value, col.intValue(), row);
+            hasChanged.set(true);
+          }
+        });
+      }
+    } finally {
+      if (hasChanged.get()) {
+        spreadSheet.saveAs(dataDir.resolve("Rechnungsliste_Budget_new.ods").toFile());
+      }
+    }
+  }
 
-	Map<String, Object> sendEmail(Map<String, String> row, Path pdfFile)
-			throws MessagingException, IOException {
-		String email = get("Email", row);
-		String vorname = get("Vorname", row);
-		String nachname = get("Name", row);
-		String emailBody = getEmailBody(row);
+  Map<String, Object> sendEmail(Map<String, String> row, Path pdfFile)
+      throws MessagingException, IOException {
+    String email = get("Email", row);
+    String vorname = get("Vorname", row);
+    String nachname = get("Name", row);
+    String emailBody = getEmailBody(row);
 
-		InternetAddress recipient = new InternetAddress(email,
-				format("%s %s", vorname, nachname));
-		Multipart mp = new MimeMultipart();
+    InternetAddress recipient = new InternetAddress(email, format("%s %s", vorname, nachname));
+    Multipart mp = new MimeMultipart();
 
-		MimeBodyPart htmlPart = new MimeBodyPart();
-		htmlPart.setText(emailBody, "ISO-8859-1");
+    MimeBodyPart htmlPart = new MimeBodyPart();
+    htmlPart.setText(emailBody, "ISO-8859-1");
 
-		mp.addBodyPart(htmlPart);
+    mp.addBodyPart(htmlPart);
 
-		// Part two is attachment
-		MimeBodyPart attachment = new MimeBodyPart();
-		DataSource source = new ByteArrayDataSource(readAllBytes(pdfFile),
-				"application/pdf");
-		attachment.setDataHandler(new DataHandler(source));
-		attachment.setFileName(RECHNUNG + ".pdf");
-		mp.addBodyPart(attachment);
+    // Part two is attachment
+    MimeBodyPart attachment = new MimeBodyPart();
+    DataSource source = new ByteArrayDataSource(readAllBytes(pdfFile), "application/pdf");
+    attachment.setDataHandler(new DataHandler(source));
+    attachment.setFileName(RECHNUNG + ".pdf");
+    mp.addBodyPart(attachment);
 
-		SMTPMessage msg = new SMTPMessage(mailSession);
-		msg.setReturnOption(RETURN_HDRS);
-		msg.setNotifyOptions(NOTIFY_FAILURE);
-		msg.setSender(sender);
-		msg.setFrom(sender);
-		msg.addRecipient(RecipientType.TO, recipient);
-		msg.setSubject("TCOW Jahresbeitrag ".concat(get("Jahr1", row)));
-		msg.setContent(mp);
-		logger.info("Sending document to {}", recipient);
-		Transport.send(msg);
-		Instant instant = date.toLocalDate().atStartOfDay()
-				.atZone(ZoneId.of("UTC")).toInstant();
-		return Collections.singletonMap("Mailed", Date.from(instant));
-	}
+    SMTPMessage msg = new SMTPMessage(mailSession);
+    msg.setReturnOption(RETURN_HDRS);
+    msg.setNotifyOptions(NOTIFY_FAILURE);
+    msg.setSender(sender);
+    msg.setFrom(sender);
+    msg.addRecipient(RecipientType.TO, recipient);
+    msg.setSubject("TCOW Jahresbeitrag ".concat(get("Jahr1", row)));
+    msg.setContent(mp);
+    logger.info("Sending document to {}", recipient);
+    Transport.send(msg, mailProperties.getProperty("mail.user"),
+        mailProperties.getProperty("mail.password"));
+    Instant instant = date.toLocalDate().atStartOfDay().atZone(ZoneId.of("UTC")).toInstant();
+    return Collections.singletonMap("Mailed", Date.from(instant));
+  }
 
-	String getEmailBody(Map<String, String> row) throws IOException {
-		try (InputStream in = getClass()
-				.getResourceAsStream(getEmailResource(row))) {
-			return new BufferedReader(
-					new InputStreamReader(in, StandardCharsets.UTF_8)).lines()
-							.map(line -> mapReplacements(line, row))
-							.collect(Collectors.joining("\n"));
-		}
-	}
+  String getEmailBody(Map<String, String> row) throws IOException {
+    try (InputStream in = getClass().getResourceAsStream(getEmailResource(row))) {
+      return new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8)).lines()
+          .map(line -> mapReplacements(line, row)).collect(Collectors.joining("\n"));
+    }
+  }
 
-	String getEmailResource(Map<String, String> row) {
-		if (get("Bezahlt", row).isEmpty()) {
-			return "BillingEmailTemplate.txt";
-		}
-		return "BillPayedEmailTemplate.txt";
-	}
+  String getEmailResource(Map<String, String> row) {
+    if (get("Bezahlt", row).isEmpty()) {
+      return "BillingEmailTemplate.txt";
+    }
+    return "BillPayedEmailTemplate.txt";
+  }
 
-	String mapReplacements(String input, Map<String, String> row) {
-		StringBuffer result = new StringBuffer();
-		Matcher matcher = replacementPattern.matcher(input);
-		while (matcher.find()) {
-			matcher.appendReplacement(result, get(matcher.group(1), row));
-		}
-		return matcher.appendTail(result).toString();
-	}
+  String mapReplacements(String input, Map<String, String> row) {
+    StringBuffer result = new StringBuffer();
+    Matcher matcher = replacementPattern.matcher(input);
+    while (matcher.find()) {
+      matcher.appendReplacement(result, get(matcher.group(1), row));
+    }
+    return matcher.appendTail(result).toString();
+  }
 
-	String get(String key, Map<String, String> row) {
-		return row.computeIfAbsent(key, k -> {
-			switch (k) {
-			case "Jahr1":
-			case "Jahr2":
-				return date.format(yearFormatter);
-			default:
-				return "<" + k + ">";
-			}
-		});
-	}
+  String get(String key, Map<String, String> row) {
+    return row.computeIfAbsent(key, k -> {
+      switch (k) {
+        case "Jahr1":
+        case "Jahr2":
+          return date.format(yearFormatter);
+        default:
+          return "<" + k + ">";
+      }
+    });
+  }
 }
